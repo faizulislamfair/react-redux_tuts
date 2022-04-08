9e375562013ae0433175c9647a67e81e8e9a8c5f
faizulislamfair <faizulislamfair@gmail.com>
2022-04-09 02:50:18 +0600

    action payload

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import store from './redux/store';
-import './App.css';
-import CakeContainer from './components/CakeContainer';
-import HooksCakeContainer from './components/HooksCakeContainer';
-import IceCreamContainer from './components/IceCreamContainer';
+import React from 'react'
+import { Provider } from 'react-redux'
+import store from './redux/store'
+import './App.css'
+import CakeContainer from './components/CakeContainer'
+import HooksCakeContainer from './components/HooksCakeContainer'
+import IceCreamContainer from './components/IceCreamContainer'
+import NewCakeContainer from './components/NewCakeContainer'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <HooksCakeContainer />
         <CakeContainer />
         <IceCreamContainer />
+        <NewCakeContainer />
       </div>
     </Provider>
   )
